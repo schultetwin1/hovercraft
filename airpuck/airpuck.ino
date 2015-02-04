07a94c32880dfeac955a7990263bd4766054766b
unknown <mttschlt@AERO-205-3.adsroot.itcs.umich.edu>
2015-02-04 14:44:20 -0500

    Removed comments

--- airpuck/airpuck.ino
+++ airpuck/airpuck.ino
@@ -1,28 +1,5 @@
 #include <Event.h>
 #include <Timer.h>
-
-/*
- Input Pullup Serial
- 
- This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a 
- digital input on pin 2 and prints the results to the serial monitor.
- 
- The circuit: 
- * Momentary switch attached from pin 2 to ground 
- * Built-in LED on pin 13
- 
- Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal 
- 20K-ohm resistor is pulled to 5V. This configuration causes the input to 
- read HIGH when the switch is open, and LOW when it is closed. 
- 
- created 14 March 2012
- by Scott Fitzgerald
- 
- http://www.arduino.cc/en/Tutorial/InputPullupSerial
- 
- This example code is in the public domain
- 
- */
  
 
 const int MOTOR_PIN = A0;
