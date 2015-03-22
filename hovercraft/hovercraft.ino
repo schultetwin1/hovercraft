a2c4f22ba57d03ed9974158ed8808fe09c7ece04
unknown <mttschlt@AERO-205-1.adsroot.itcs.umich.edu>
2015-03-22 16:07:58 -0400

    Servos and remote up

--- hovercraft/hovercraft.ino
+++ hovercraft/hovercraft.ino
@@ -2,8 +2,8 @@
 
 const int LIFT_MOTOR_PIN = A0;
 const int PULL_MOTOR_PIN = A2;
-const int MOTOR_SERVO_PIN = 9;
-const int RUDDER_SERVO_PIN = 10;
+const int MOTOR_SERVO_PIN = 6;
+const int RUDDER_SERVO_PIN = 5;
 const int POT_PIN = A1;
 const int LED_PIN = 13;
 const int TIMER_INTERRUPT = 0;
@@ -11,23 +11,31 @@ const byte MOTOR_ON  = 255;
 const byte MOTOR_OFF = 0;
 const int ANALOG_READ_MAX = 1023;
 const int ANALOG_READ_MIN = 0;
-const int CHANNEL_PINS [5] = {
+
+// 25 milliseconds
+const int PULSEIN_TIMEOUT = 25000;
+
+/**
+ * Channel 3 = Lift
+ * Channel 2 = Pull (Thrust forward)
+ * Channel 1 = Direction
+ */
+const int CHANNEL_PINS [4] = {
   -1, /* Channel 0 does not exist */
-  4,
-  5,
-  6,
-  7
+  7,
+  8,
+  9,
 };
 
 Servo pull;
+unsigned last_pull_us = 1500;
 Servo rudder;
+unsigned last_rudder_us = 1500;
 
 void setup(){
   
   //start serial connection
-#ifdef DEBUG
   Serial.begin(9600);
-#endif
   
   pinMode(LED_PIN, OUTPUT);
   pinMode(LIFT_MOTOR_PIN, OUTPUT);
@@ -38,27 +46,37 @@ void setup(){
   }
   
   pull.attach(MOTOR_SERVO_PIN);
-  pull.write(90);
+  pull.writeMicroseconds(1500);
   rudder.attach(RUDDER_SERVO_PIN);
-  rudder.write(90);
+  rudder.writeMicroseconds(1500);
 }
 
 void loop(){
   // Read from the reciever
-  int lift = pulseIn(CHANNEL_PINS[3], HIGH);
-  int thrust = pulseIn(CHANNEL_PINS[3], HIGH);
-  int dir = pulseIn(CHANNEL_PINS[3], HIGH);
+  int lift = pulseIn(CHANNEL_PINS[3], HIGH, PULSEIN_TIMEOUT);
+  int thrust = pulseIn(CHANNEL_PINS[2], HIGH, PULSEIN_TIMEOUT);
+  int dir = pulseIn(CHANNEL_PINS[1], HIGH, PULSEIN_TIMEOUT);
   
-  analogWrite(LIFT_MOTOR_PIN, lift);
-  analogWrite(THRUST_MOTOR_PIN, thrust);
-  rudder.write(dir);
-  
-  
-  Serial.print("Ch3 Value: ");
-  Serial.print(ch3Value);
-  Serial.print(" Time: ");
-  Serial.print(millis());
+  Serial.print("Lift: ");
+  Serial.print(lift);
+  Serial.print(" Pull: ");
+  Serial.print(thrust);
+  Serial.print(" Dir: ");
+  Serial.print(dir);
   Serial.print("\n");
+  
+  analogWrite(LIFT_MOTOR_PIN, map(constrain(lift, 1000, 1850), 1000, 1850, 0, 255));
+  analogWrite(PULL_MOTOR_PIN, map(constrain(thrust, 1150, 1950), 1150, 1950, 0, 255));
+  /*
+  if (abs(last_pull_us - dir) > 100) {
+    pull.writeMicroseconds(constrain(dir, 1060, 1860));
+    last_pull_us = dir;
+  }
+  if (abs(last_rudder_us - dir) > 100) {
+    rudder.writeMicroseconds(constrain(dir, 1060, 1860));
+    last_rudder_us = dir;
+  }
+  */
 }
 
 void turn_off_motor() {
