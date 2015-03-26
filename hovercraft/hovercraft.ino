94344e9fd52490fed20d4edb43216a3999cd2e84
Matt Schulte <schultetwin1@gmail.com>
2015-03-26 01:05:04 -0400

    Moved pin and cleaned up magic numbers

--- hovercraft/hovercraft.ino
+++ hovercraft/hovercraft.ino
@@ -1,16 +1,36 @@
 #include <Servo.h>
 
-const int LIFT_MOTOR_PIN = 10;
+// These pin numbers have been choose carefully
+// Please ask Matt before changing
+// DO NOT USE analogWrite on pins 9 or 10, it will not work
+// See (http://arduino.cc/en/reference/servo) for explination
+
+// Pin 3 and Pin 11 use Timer 2
+const int LIFT_MOTOR_PIN = 3;
 const int PULL_MOTOR_PIN = 11;
+
 const int MOTOR_SERVO_PIN = 6;
 const int RUDDER_SERVO_PIN = 5;
-const int POT_PIN = A1;
+
 const int LED_PIN = 13;
-const int TIMER_INTERRUPT = 0;
-const byte MOTOR_ON  = 255;
-const byte MOTOR_OFF = 0;
-const int ANALOG_READ_MAX = 1023;
-const int ANALOG_READ_MIN = 0;
+
+const int MAX_LIFT_OUTPUT = 200;
+const int MIN_LIFT_OUTPUT = 0;
+const int MAX_LIFT_INPUT  = 1850;
+const int MIN_LIFT_INPUT  = 1000;
+
+const int MAX_THRUST_OUTPUT = 255;
+const int MIN_THRUST_OUTPUT = 0;
+const int MAX_THRUST_INPUT  = 1900;
+const int MIN_THRUST_INPUT  = 1550;
+
+const int MAX_RUDDER_OUTPUT    = 2100;
+const int MIN_RUDDER_OUTPUT    = 900;
+const int MAX_MOTOR_DIR_OUTPUT = 2100;
+const int MIN_MOTOR_DIR_OUTPUT = 900;
+const int MAX_DIR_INPUT        = 1860;
+const int MIN_DIR_INPUT        = 1060;
+
 
 // 25 milliseconds
 const int PULSEIN_TIMEOUT = 25000;
@@ -98,7 +118,6 @@ void setup(){
   pinMode(LED_PIN, OUTPUT);
   pinMode(LIFT_MOTOR_PIN, OUTPUT);
   pinMode(PULL_MOTOR_PIN, OUTPUT);
-  pinMode(POT_PIN, INPUT);
   for (int i = 1; i < (sizeof(CHANNEL_PINS) / sizeof(CHANNEL_PINS[0]));  i++) {
     pinMode(CHANNEL_PINS[i], INPUT);
   }
@@ -113,25 +132,31 @@ void loop(){
   // Read from the reciever
   int lift = pulseInLong(CHANNEL_PINS[3], HIGH, PULSEIN_TIMEOUT);
   if (lift != 0) {
-    lift = map(constrain(lift, 1000, 1850), 1000, 1850, 0, 255);
+    lift = constrain(lift, MIN_LIFT_INPUT, MAX_LIFT_INPUT);
+    lift = map(lift, MIN_LIFT_INPUT, MAX_LIFT_INPUT, MIN_LIFT_OUTPUT, MAX_LIFT_OUTPUT);
   }
   analogWrite(LIFT_MOTOR_PIN, lift);
     
   int thrust = pulseInLong(CHANNEL_PINS[2], HIGH, PULSEIN_TIMEOUT);
   if (thrust != 0) {
-    thrust = map(constrain(thrust, 1500, 1950), 1500, 1950, 0, 255);
+    thrust = constrain(thrust, MIN_THRUST_INPUT, MAX_THRUST_INPUT);
+    thrust = map(thrust, MIN_THRUST_INPUT, MAX_THRUST_INPUT, MIN_THRUST_OUTPUT, MAX_THRUST_OUTPUT);
   }
   analogWrite(PULL_MOTOR_PIN, thrust);
   
   int dir = pulseInLong(CHANNEL_PINS[1], HIGH, PULSEIN_TIMEOUT);
+  int rudder_dir;
+  int motor_dir;
   if (dir != 0) {
-    dir = map(constrain(dir, 1060, 1860), 1060, 1860, 900, 2100);
+    dir = constrain(dir, MIN_DIR_INPUT, MAX_DIR_INPUT);
+    rudder_dir = map(dir, MIN_DIR_INPUT, MAX_DIR_INPUT, MIN_RUDDER_OUTPUT, MAX_RUDDER_OUTPUT);
+    motor_dir  = map(dir, MIN_DIR_INPUT, MAX_DIR_INPUT, MIN_MOTOR_DIR_OUTPUT, MAX_MOTOR_DIR_OUTPUT);
   } else {
     dir = 1500;
   }
   
-  pull.writeMicroseconds(dir);
-  rudder.writeMicroseconds(dir);
+  pull.writeMicroseconds(motor_dir);
+  rudder.writeMicroseconds(rudder_dir);
   
   Serial.print("Millis: ");
   Serial.print(millis());
@@ -142,8 +167,4 @@ void loop(){
   Serial.print(" Dir: ");
   Serial.print(dir);
   Serial.print("\n");
-}
-
-void turn_off_motor() {
-  analogWrite(LIFT_MOTOR_PIN, MOTOR_OFF);
 }
