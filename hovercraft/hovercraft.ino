16068e3d40f67fc1c82a25fd483a4a85e2a62ced
unknown <mttschlt@AERO-205-6.adsroot.itcs.umich.edu>
2015-04-02 19:38:20 -0400

    Using PPM and H-Bridge

--- hovercraft/hovercraft.ino
+++ hovercraft/hovercraft.ino
@@ -8,7 +8,8 @@
 // Pin 3 and Pin 11 use Timer 2
 const int LIFT_MOTOR_PIN = 3;
 const int THRUST_MOTOR_PIN = 11;
-const int BOOST_PIN = 12;
+const int THRUST_DIR_PIN = 12;
+const int PPM_PIN = 2; // !! MUST BE ON PIN 2 !! 
 
 const int MOTOR_SERVO_PIN = 6;
 
@@ -18,22 +19,23 @@ const int LED_PIN = 13;
 // Output between 0 - 200 (not 255 to save the motor)
 int MAX_LIFT_OUTPUT = 200;
 const int MIN_LIFT_OUTPUT = 0;
-const int MAX_LIFT_INPUT  = 1850;
-const int MIN_LIFT_INPUT  = 1050;
+const int MAX_LIFT_INPUT  = 1160;
+const int MIN_LIFT_INPUT  = 572;
 
 // Thrust Input will be between 1550us (halfway) and 1900us
 // Output between 0 - 255
 const int MAX_THRUST_OUTPUT = 255;
 const int MIN_THRUST_OUTPUT = 0;
-const int MAX_THRUST_INPUT  = 1900;
-const int MIN_THRUST_INPUT  = 1600;
+const int MAX_THRUST_INPUT  = 1584;
+const int MIN_THRUST_INPUT  = 760;
+const int MID_THRUST_INPUT  = 1180;
 
 // Lift Input will be between 1060 and 1860us
 // Output between 900 - 2100 us
 const int MAX_MOTOR_DIR_OUTPUT = 2000;
 const int MIN_MOTOR_DIR_OUTPUT = 1000;
-const int MAX_DIR_INPUT        = 1860;
-const int MIN_DIR_INPUT        = 1060;
+const int MAX_DIR_INPUT        = 1592;
+const int MIN_DIR_INPUT        = 772;
 
 
 // 25 milliseconds
@@ -45,21 +47,15 @@ const int PULSEIN_TIMEOUT = 25000;
  * Channel 1 = Direction
  */
 
-enum Channels {
-  LIFT_CHANNEL = 3,
-  THRUST_CHANNEL = 2,
-  DIR_CHANNEL = 1,
-  BOOST_CHANNEL = 5
-};  
-  
-const int CHANNEL_PINS [6] = {
-  -1, /* Channel 0 does not exist */
-  7,
-  8,
-  9,
-  -1, /* Not using channel 4 */
-  12
-};
+
+// Channel 0: 772 - 1592 (dir)
+// Channel 1: 760 - 1584 (thrust) (mid = 1180)
+// Channel 2: 572 - 1400 (lift)
+// Channel 3: 764 - 1584 (not used)
+// Channel 4: 580, 1380
+volatile uint16_t curPulse = 0;
+volatile uint32_t PULSESTART = 0;
+volatile uint32_t PULSES[5] = {0,0,0,0,0};
 
 const int FRAME_SIZE = 5;
 int CHANNEL_FILTER[FRAME_SIZE] = {1500};
@@ -125,61 +121,80 @@ unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout) {
   return micros() - start;
 }
 
+void ISRPIN2(){
+  // Handle PPM pin changes
+   if(digitalRead(PPM_PIN) == HIGH){
+     //rising edge
+     PULSESTART = micros();
+   }else{
+     //falling edge
+     uint16_t time = micros() - PULSESTART;
+     if(time > 5000){
+       // Greater than 5ms, therefore it's the gap.
+       curPulse = 0;
+       return;
+     }
+     if(curPulse > 4){
+       // we have a problem, controller disconnected?
+       // continue current state
+       return; 
+     }
+     // Assign the pulse and increment the count.
+     PULSES[curPulse++] = time;
+   }
+}
+
 void setup(){
   
   //start serial connection
   Serial.begin(9600);
 
   // @TODO: Needs to pulse at 1Hz  
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);  
+  pinMode(THRUST_DIR_PIN, OUTPUT);
+  digitalWrite(THRUST_DIR_PIN, LOW);
   
   // Setup lift and thrust output pins
   pinMode(LIFT_MOTOR_PIN, OUTPUT);
   pinMode(THRUST_MOTOR_PIN, OUTPUT);
-  for (int i = 1; i < (sizeof(CHANNEL_PINS) / sizeof(CHANNEL_PINS[0]));  i++) {
-    pinMode(CHANNEL_PINS[i], INPUT);
-  }
+  pinMode(PPM_PIN, INPUT);
   
   thrust_servo.attach(MOTOR_SERVO_PIN);
   thrust_servo.writeMicroseconds(1500);
+  
+  attachInterrupt(0,ISRPIN2,CHANGE);
 }
 
-void loop(){
-  // Read lift from the reciever and output to motor
-  int all_read = 1;
-  int lift = pulseInLong(CHANNEL_PINS[LIFT_CHANNEL], HIGH, PULSEIN_TIMEOUT);
-  Serial.print(" Lift: ");
-  Serial.print(lift);
+void lift() {
+  //int lift = pulseInLong(CHANNEL_PINS[LIFT_CHANNEL], HIGH, PULSEIN_TIMEOUT);
+  int lift = PULSES[2];
   if (lift != 0) {
     lift = constrain(lift, MIN_LIFT_INPUT, MAX_LIFT_INPUT);
     lift = map(lift, MIN_LIFT_INPUT, MAX_LIFT_INPUT, MIN_LIFT_OUTPUT, MAX_LIFT_OUTPUT);
   }
   analogWrite(LIFT_MOTOR_PIN, lift);
-  
-  
-  // Read thrust from receiver and output to motor  
-  int thrust = pulseInLong(CHANNEL_PINS[THRUST_CHANNEL], HIGH, PULSEIN_TIMEOUT);
-  Serial.print(" Thrust: ");
-  Serial.print(thrust);
+}
+
+void thrust() {
+   // Read thrust from receiver and output to motor  
+  //int thrust = pulseInLong(CHANNEL_PINS[THRUST_CHANNEL], HIGH, PULSEIN_TIMEOUT);
+  int thrust = PULSES[1];
   if (thrust != 0) {
-    thrust = constrain(thrust, MIN_THRUST_INPUT, MAX_THRUST_INPUT);
-    thrust = map(thrust, MIN_THRUST_INPUT, MAX_THRUST_INPUT, MIN_THRUST_OUTPUT, MAX_THRUST_OUTPUT);
+    if (thrust < MID_THRUST_INPUT - 40) {
+      thrust = map(thrust, MID_THRUST_INPUT, MIN_THRUST_INPUT, MID_THRUST_INPUT, MAX_THRUST_INPUT);
+      digitalWrite(THRUST_DIR_PIN, HIGH);
+    } else {
+      digitalWrite(THRUST_DIR_PIN, LOW);
+    }
+    thrust = constrain(thrust, MID_THRUST_INPUT, MAX_THRUST_INPUT);
+    thrust = map(thrust, MID_THRUST_INPUT, MAX_THRUST_INPUT, MIN_THRUST_OUTPUT, MAX_THRUST_OUTPUT);
   }
   analogWrite(THRUST_MOTOR_PIN, thrust);
-  
-  // Read in direction from receiver and output to both servos
-  int dir = pulseInLong(CHANNEL_PINS[DIR_CHANNEL], HIGH, PULSEIN_TIMEOUT);
-  int thrust_dir;
-  if (dir != 0) {
-    dir = constrain(dir, MIN_DIR_INPUT, MAX_DIR_INPUT);
-    thrust_dir  = map(dir, MIN_DIR_INPUT, MAX_DIR_INPUT, MAX_MOTOR_DIR_OUTPUT, MIN_MOTOR_DIR_OUTPUT);
-  } else {
-    thrust_dir = 1500;
-  }
-  
-  thrust_servo.writeMicroseconds(thrust_dir);
-  
-  int boost = pulseInLong(CHANNEL_PINS[BOOST_CHANNEL], HIGH, PULSEIN_TIMEOUT);
+}
+
+void boost() {
+  //int boost = pulseInLong(CHANNEL_PINS[BOOST_CHANNEL], HIGH, PULSEIN_TIMEOUT);
+  int boost = PULSES[4];
   
   if (boost < 1500) {
     MAX_LIFT_OUTPUT = 255;
@@ -188,8 +203,35 @@ void loop(){
     MAX_LIFT_OUTPUT = 200;
     digitalWrite(LED_PIN, LOW);
   }
+}
+
+void dir() {
+  // Read in direction from receiver and output to both servos
+  //int dir = pulseInLong(CHANNEL_PINS[DIR_CHANNEL], HIGH, PULSEIN_TIMEOUT);
+  int dir = PULSES[0];
+  int thrust_dir;
+  if (dir != 0) {
+    dir = constrain(dir, MIN_DIR_INPUT, MAX_DIR_INPUT);
+    thrust_dir  = map(dir, MIN_DIR_INPUT, MAX_DIR_INPUT, MAX_MOTOR_DIR_OUTPUT, MIN_MOTOR_DIR_OUTPUT);
+  } else {
+    thrust_dir = 1500;
+  }
+  thrust_servo.writeMicroseconds(thrust_dir);
   
-  Serial.print(" Boost: ");
-  Serial.print(boost);
-  Serial.print("\n");
+}
+
+void loop(){
+  // Read lift from the reciever and output to motor
+  //boost();
+  lift();
+  thrust();
+  dir();
+  if (micros() - PULSESTART > 30000) {
+    for (int i = 0; i < 5; i++) {
+      PULSES[i] = 0;
+    }
+  }
+  
+  delay(25);
+  
 }
